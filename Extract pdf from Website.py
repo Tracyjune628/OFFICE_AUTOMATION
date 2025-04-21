d2806461aa6147b4a24adb413efbe39cca077519
Tracyjune628 <130264023+Tracyjune628@users.noreply.github.com>
2025-04-20 18:43:51 -0700

    Update Extract pdf from Website.py

--- Extract pdf from Website.py
+++ Extract pdf from Website.py
@@ -85,13 +85,14 @@ def extract_url_pdf(input_url,folder_path=os.getcwd()):
 # In[5]:
 
 
-extract_url_pdf(input_url="https://www.icai.org/post.html?post_id=17843")
+extract_url_pdf(input_url="https://www.marxists.org/history/usa/pubs/black-panther/")
 
 
 # In[6]:
 
 
 extract_url_pdf(input_url="https://cbic-gst.gov.in/central-tax-notifications.html")
+
 
 # https://cbic-gst.gov.in/central-tax-notifications.html
 
